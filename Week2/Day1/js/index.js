549a89f2c102d71b19a030971475d9a5a196275c
Rituparna Behura <152375257+ritu123p@users.noreply.github.com>
2025-02-19 09:50:10 +0530

    Update index.js

--- Week2/Day1/js/index.js
+++ Week2/Day1/js/index.js
@@ -2,68 +2,6 @@ const inputBox = document.getElementById('inputBox');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
 
-// //Add functionality
-// const addTodo = () => {
-//     const inputText = inputBox.value;
-//     if(inputText.length < 3){
-//         alert("add atleast 3 items")
-//     }
-//     else{
-//         const li = document.createElement('li');
-//         const p = document.createElement('p');
-//         p.innerHTML = inputText;
-//         li.appendChild(p);
-
-//         //Edit button 
-//         const editBtn = document.createElement("button");
-//         editBtn.innerText = "Edit";
-//         editBtn.classList.add("btn", "editBtn");
-//         li.appendChild(editBtn);
-
-//         //Delete button
-//         const deleteBtn = document.createElement("button")
-//         deleteBtn.innerText = "Delete";
-//         deleteBtn.classList.add("btn", "deleteBtn");
-//         li.appendChild(deleteBtn);
-
-//         todoList.appendChild(li);
-//         addLocalStorage(inputText);
-//     }
-// }
-// const addLocalStorage = (todo) => {
-//     let todos = JSON.parse(localStorage.getItem("todos")) || [];
-//     todos.push(todo);
-//     localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-// const getLocalTodo = () => {
-//     let todos = JSON.parse(localStorage.getItem("todos")) || [];
-//     todos.forEach(todo =>{
-//         const li = document.createElement('li');
-//         const p = document.createElement('p');
-//         p.innerHTML = todo;
-//         li.appendChild(p);
-//         todoList.appendChild(li);
-//     })
-// }
-
-
-// //Edit Functionality
-// const editTodo1 = null;
-// const updateTodo = (eventObj) =>{
-//     if(addBtn.value == "Edit"){
-//         inputBox.value = eventObj.target.previousElementSibling.innerHTML;
-//         inputBox.focus();
-//         addBtn.value = "Edit";
-//         editTodo1 = eventObj;
-//     }
-// }
-
-// document.addEventListener('DOMContentLoaded', getLocalTodo)
-// addBtn.addEventListener('click', addTodo)
-
-
-
 let editTodo = null;
 
 const saveLocalTodos = (todo) => {
